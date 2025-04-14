f94f7de601dab8a54c7c823f389f976d6d7569f5
messoblews <vohoang200537@gmail.com>
2025-04-13 20:14:50 -0400

    The new main.cpp file for Windows Command Prompt
    This new and improved main.cpp file helps run my text-based fighting game if you are running it from Windows Command Prompt instead of Visual Studio Code.

--- main.cpp
+++ main.cpp
@@ -7,5 +7,13 @@ using namespace std;
 
 int main() {
     GameMenu();
+
+    // Add this part to prevent the window from closing
+    cout << "Press Enter to close the game..." << endl;
+    cout << "To play again, open the main application found in my GitHub." << endl;
+    cout << "Nice playing with you!" << endl;
+    cin.ignore(); // Flush leftover newline from previous input
+    cin.get();    // Waits for Enter key
+
     return 0;
 }
